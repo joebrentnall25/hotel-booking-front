9f029e76f57b0f93b0c75292be2d7b667a71c67f
Joe Brentnall <joebrentnall@Joes-MBP.lan>
2022-01-14 09:57:06 +0000

    Finished routing page

--- src/components/Router/Routing.jsx
+++ src/components/Router/Routing.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const Routing = () => {
     return (
-        <></>
+        <Routes>
+            <Route path="/user" component={<h1>User</h1>}/>
+            <Route path="/rooms" component={<h1>Rooms</h1>}/>
+            <Route path="/booking" component={<h1>Bookings</h1>}/>
+        </Routes>
     )
 }
 
